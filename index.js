94d6528a2ff827ceeea347499e6cac4bd4821710
Jonathan Viera <156127051+jviera100@users.noreply.github.com>
2024-03-25 12:17:22 -0300

    :sparkles: agrega 10 usuarios

--- index.js
+++ index.js
@@ -9,52 +9,52 @@ moment.locale('es'); // idioma fecha
 const app = express();
 const PORT = 3001;
 
+// Array para almacenar usuarios
 let users = [];
 
-// Función para registrar un usuario desde la API Random User
-async function registerUserFromAPI() {
-  try {
-    const response = await axios.get(`https://randomuser.me/api/`);
-    const result = response.data.results[0];
-    const user = {
-      id: uuid.v4(),
-      firstName: result.name.first,
-      lastName: result.name.last,
-      gender: result.gender,
-      timestamp: moment().format('DD-MM-YYYY HH:mm:ss')
-    };
-    users.push(user);
-    console.log(chalk.underline.bgWhiteBright.blue.bold.italic(JSON.stringify({ message: 'Usuarios registrados desde la API:', users }, null, 2)));
-  } catch (error) {
-    console.error(chalk.underline.bgWhiteBright.blue.bold.italic(JSON.stringify({ message: 'Error al registrar usuario desde la API:', error}, null, 2)));
-  }
-}         
-// Ruta para consultar usuarios registrados
-app.get('/users', (req, res) => {
-  try {
-    const females = _.filter(users, { gender: 'female' });
-    const males = _.filter(users, { gender: 'male' });
-    const groupedUsers = { females, males };
-    res.json(groupedUsers);
-    console.log(chalk.underline.bgWhiteBright.blue.bold.italic(JSON.stringify({ message: 'Usuarios registrados desde la API:', users }, null, 2)));
-  } catch (error) {
-    console.error(chalk.underline.bgWhiteBright.blue.bold.italic(JSON.stringify({ message: 'Error al registrar usuario desde la API:', error}, null, 2)));    res.status(500).json({ error: 'Error al consultar usuarios desde la API' });
-  }
-});
-
+// Función para registrar usuarios desde la API Random User
+async function registerUsersFromAPI(numUsers) {
+    try {
+        const response = await axios.get(`https://randomuser.me/api/?results=${numUsers}`);
+        const {results}= response.data; //desestructuracion
+        results.forEach(result => {
+            const user = {
+                id: uuid.v4(),
+                firstName: result.name.first,
+                lastName: result.name.last,
+                gender: result.gender,
+                timestamp: moment().format('DD-MM-YYYY HH:mm:ss')
+            };
+            users.push(user);
+        });
+        console.log(chalk.underline.bgWhiteBright.blue.bold.italic(JSON.stringify({ message: 'Usuarios registrados desde la API:', users }, null, 2)));
+    } catch (error) {
+        console.error(chalk.underline.bgWhiteBright.blue.bold.italic(JSON.stringify({ message: 'Error al registrar usuarios desde la API:', error}, null, 2)));
+    }
+}
+// Llamar a la función para registrar usuarios desde la API Random User
+registerUsersFromAPI(10); // Registrar 10 usuarios aleatorios
+//-------------------------------------------------------------------------
 // Middleware para imprimir lista de usuarios en la consola con Chalk
 function logUsers(req, res, next) { 
-  console.log(chalk.underline.bgWhiteBright.blue.bold.italic(JSON.stringify({ message: 'Usuarios registrados desde la API:', users }, null, 2)));
+  console.log(chalk.underline.bgWhiteBright.blue.bold.italic('Lista de usuarios registrados:'));
+  console.log(chalk.underline.bgWhiteBright.bold.italic(JSON.stringify(users)));
   next();
 }
-
+//el console.log del midleware aparece en el terminal cuando cargo la ruta de la pagina web, se consume la API
 // Aplicar middleware globalmente para todas las solicitudes
 app.use(logUsers);
 
-// Programar la llamada a la función para registrar usuarios desde la API Random User
-setInterval(registerUserFromAPI, 5000); // Registrar 1 usuario aleatorio cada 5 segundos
+// Ruta para consultar usuarios registrados
+app.get('/users', (req, res) => {
+  // Dividir usuarios por sexo usando Lodash
+  const females = _.filter(users, { gender: 'female' });
+  const males = _.filter(users, { gender: 'male' });
+  const groupedUsers = { females, males };
+  res.json(groupedUsers);
+});
 
 // Iniciar el servidor con Nodemon
 app.listen(PORT, () => { 
-  console.log(chalk.bgWhiteBright.blue.bold.italic(`🔥🔥🔥🔥🔥 Servidor corriendo en el puerto http://localhost:${PORT}`));
+  console.log(chalk.underline.bgWhiteBright.blue.bold.italic(`🔥🔥🔥🔥🔥Servidor corriendo en el puerto🔥🔥🔥🔥🔥http://localhost:${PORT}`));
 });
